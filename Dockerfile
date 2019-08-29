c6b839b546f4b6a9e1e3ac87493c0297b5902676
Pritesh kumar <pritesh.kumar2@gmail.com>
2019-08-29 19:48:43 +0530

    nodb changes

--- Dockerfile
+++ Dockerfile
@@ -5,16 +5,16 @@ label author="pritesh.kumar2@gmail.com"
 label usesdb="mysql:8.0"
 ENV npmuse=1
 ENV node_env="testing"
-ARG username="jenkins"
-ARG appdir="nodeapps"
-run useradd -ms /bin/bash $username
-run mkdir -p /home/${username}/${appdir}/
+#ARG username="jenkins"
+#ARG appdir="nodeapps"
+#run useradd -ms /bin/bash $username
+#run mkdir -p /home/${username}/${appdir}/
 run apt-get update -y && \
     apt-get install -y git
 #USER $username
-workdir /home/${username}/${appdir}
-run git clone https://github.com/priteshkumar/movielist.git
+#workdir /home/${username}/${appdir}
+run git clone https://github.com/priteshkumar/movielist.git -b nodb
 run cd movielist && npm install
 expose 3000/tcp
-entrypoint ["/bin/sh",-"-c","node --version"]
+#entrypoint ["/bin/sh",-"-c","node server.js"]
 
--- server.js
+++ server.js
@@ -13,93 +13,15 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
-/*
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "mavpks",
-  database: "moviePlannerDB"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
-*/
+//var mysql = require("mysql");
 
 // Use Handlebars to render the main index.html page with the todos in it.
 app.get("/", function(req, res) {
- /* connection.query("SELECT * FROM movies;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }*/
 
     res.render("index", { movies: [{"id":1,"movie":"smokinaces"},{"id":2,"movie":"exam"}] });
-  //});
 });
 
 
-// Create a new todo
-app.post("/movies", function(req, res) {
-  connection.query("INSERT INTO movies (movie) VALUES (?)", [req.body.movie], function(err, result) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    // Send back the ID of the new todo
-    res.json({ id: result.insertId });
-    console.log({ id: result.insertId });
-  });
-});
-
-
-// Retrieve all todos
-app.get("/movies", function(req, res) {
-  connection.query("SELECT * FROM movies;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    res.json(data);
-  });
-});
-
-
-// Update a todo
-app.put("/movies/:id", function(req, res) {
-  connection.query("UPDATE movies SET movie = ? WHERE id = ?", [req.body.movie, req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server faliure
-      return res.status(500).end();
-    } else if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-
-// Delete a todo
-app.delete("/movies/:id", function(req, res) {
-  connection.query("DELETE FROM movies WHERE id = ?", [req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server faliure
-      return res.status(500).end();
-    } else if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
 
 
 app.listen(port, function() {
